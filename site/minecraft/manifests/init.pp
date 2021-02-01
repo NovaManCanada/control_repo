4ba1624e59a625b1334aea5aa2632ab87b7157a5
NovaManCanada <jamesurie@storm.ca>
2021-02-01 10:07:47 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
+class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
   $install_dir = '/opt/minecraft'
+  ) {
   file {'/opt/minecraft':
     ensure => directory,
   }
